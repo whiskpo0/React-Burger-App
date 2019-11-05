3534b4bf6e40780f0842f7517786522e0e843515
Adam M <whiskypo082@gmail.com>
2019-11-05 07:16:29 -0600

    updated Logo sizing

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ const sideDrawer = (props) => {
     //...
     return (
         <div className={classes.sideDrawer}>
-            <Logo height="11%"/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
             <nav>
                 <NavigationItems />
             </nav>
